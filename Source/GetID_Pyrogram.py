0bc8cd6a58dbf3f6ae576bbb3c0c6d507f5328b3
Matt Novoselov <matveynovoselov@gmail.com>
2024-03-07 12:34:25 +0100

    Moved source files to separate folder

---
+++ Source/GetID_Pyrogram.py
@@ -0,0 +1,33 @@
+from pyrogram import Client
+from pyrogram.raw.functions.messages import GetStickerSet
+from pyrogram.raw.types import InputStickerSetShortName
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+session_string = os.getenv('SESSION_STRING')
+app = Client("my_account", session_string=session_string)
+
+
+async def get_id_by_sticker(set_short_name):
+    try:
+        sticker_set = await app.invoke(
+            query=GetStickerSet(
+                hash=0,
+                stickerset=InputStickerSetShortName(short_name=set_short_name)
+            )
+        )
+        return sticker_set.set.id >> 32
+    except Exception as e:
+        print("We were unable to get id of sticker set, because:", e)
+
+
+async def get_id_by_emoji(custom_emoji_ids):
+    stickers = await app.get_custom_emoji_stickers(
+        custom_emoji_ids=custom_emoji_ids
+    )
+    UserIDs = []
+    for i in range(len(stickers)):
+        UserIDs.append(await get_id_by_sticker(stickers[i].set_name))
+
+    return UserIDs
